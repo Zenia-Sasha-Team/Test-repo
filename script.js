f7109ad1cc6fcbf05ce103838b8d911100f76829
YevheniiVdovin <32207802+YevheniiVdovin@users.noreply.github.com>
2020-11-18 14:44:55 +0100

    JS Basics, completed 2.1 - 2.4

--- script.js
+++ script.js
@@ -1,2 +1,62 @@
-document.body.innerHTML = '<h1>Hello from JavaScript</h1>'
-console.log('Test !!!!!!!!!!!')
+// alert ('Привет')
+// alert ('Мир')
+// alert (3 + 1+ 2);
+
+
+// alert("Сейчас будет ошибка");
+//      [1,2].forEach(alert)
+
+// "use strict"
+// alert("Some code")
+
+// let message;
+// message = 'Hello' //сохранить строку
+// alert(message); //показывает содержимое переменной
+
+// let user = 'Zenia',
+//     age = 30,
+//     message = 'lern JavaScript';
+
+// let message;
+// message = 'Hello';
+// message = 'World';
+// alert(message);
+
+// let hello = 'Hello world';
+// let message;
+// message = hello;
+// alert(hello);
+// alert(message);
+
+// let userName;
+// let test123;
+
+// let $ = 1; //обьявили переменную с именем "$"
+// let _ = 2; //обьявили переменную с именем "_"
+// alert($ + _);
+
+// let let, class, return, function //нельзя использовать эти имена в переменных
+
+// const myBirthday = "19.12.1989";
+//     //myBirthday = "21.21.21";
+// alert(myBirthday);
+
+// const COLOR_RED = "#F00";
+// const COLOR_GREEN = "#0F0";
+// const COLOR_BLUE = "#00F";
+// const COLOR_ORANGE = "#FF7F00"
+// //когда нам нужно выбрать цвет
+// let color = COLOR_ORANGE;
+// alert(color);
+
+                // Task 1 - Working with variables
+
+// let name = "Zenia";
+// let admin;
+// admin = name;
+// alert(admin);
+
+                // Task 2 - Giving the right name
+
+let Earth = "Земля";
+let currentUserName = "Zenia";
