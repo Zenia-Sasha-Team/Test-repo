ab6682547674da8faf82c7498f38fc2fce1b9391
YevheniiVdovin <32207802+YevheniiVdovin@users.noreply.github.com>
2020-11-21 20:45:03 +0100

    comleted

--- script.js
+++ script.js
@@ -127,8 +127,55 @@
 
 //             JS Basics [module 2] - pt.2.7 - 2.12 !!!!!!!
 
-let value = true;
-alert (typeof value);
+// let value = true;
+// alert (typeof value);
 
-value = String(value);
-alert(typeof value);
+// value = String(value);
+// alert(typeof value);
+
+// alert(6 / 2);  
+
+// let str = "123";
+// alert(typeof str);
+
+// let num = Number(str);
+// alert(typeof num);
+
+// let age = Number("любая строка");
+// alert(age);
+
+// alert(Number("   123   "));
+// alert(Number("123z"));
+// alert(Number(true));
+// alert(Number(false));
+
+// alert(Boolean(1));
+// alert(Boolean(0));
+
+// alert(Boolean("hi"));
+// alert(Boolean(""));
+
+// let x = 1;
+// x = -x;
+// alert(x);
+
+
+// let x = 144;
+// alert( x ** (1/2));
+// // alert(x % y);
+
+// let s = "my " + "string";
+// alert(s);
+
+// let x = -3;
+// alert(+x);
+
+
+// let apples = "12";
+// let oranges = "3";
+// alert(+apples + +oranges);
+
+let n = 2;
+n = n + 5;
+n = n * 2;
+alert(n);
