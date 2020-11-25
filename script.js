8c20a9aba459b44af219a84154d643c4bf88f0e5
YevheniiVdovin <32207802+YevheniiVdovin@users.noreply.github.com>
2020-11-25 15:00:34 +0100

    test sourcetree

--- script.js
+++ script.js
@@ -333,8 +333,49 @@
 
 //Task 4
 
-let message = (login == 'Сотрудник') ? 'Привет' :
-(login == 'Директор') ? 'Здравствуйте' : 
-(login == '') ? '' : '';
+// let message = (login == 'Сотрудник') ? 'Привет' :
+// (login == 'Директор') ? 'Здравствуйте' : 
+// (login == '') ? 'Нет логина' : '';
+ 
+                             //   2.11
 
+ //                            result = a || b;
+
+// alert(true || true);  //true
+// alert(false || true); //true
+// alert(true || false); //true
+// alert(false || false) //false
+
+// let hour = 9;
+// if (hour < 10 || hour > 18) {
+//     alert("Офис закрыт");
+// }
+
+// let hour = 12;
+// let isWeekend = true;
+
+// if (hour < 10 || hour > 18 || isWeekend) {
+//   alert( 'Офис закрыт.' );    // это выходной
+// }
+
+// alert(1 || 0);   //1
+// alert( true || 'no matter what' ); // true
+// alert( null || 1 ); // 1 (первое истинное значение)
+// alert( null || 0 || 1 ); // 1 (первое истинное значение)
+// alert( undefined || null || 0 ); // 0 (поскольку все ложно, возвращается последнее значение)
+
+// let currentUser = null;
+// let defaultUser = "John";
+// let name = currentUser || defaultUser || "unnamed";
+// alert( name ); // выбирается "John" – первое истинное значение
+
+// let x;
+// true || (x = 1);
+// alert(x); // undefined, потому что (x = 1) не вычисляется
+
+// let x;
+// false || (x = 1);
+// alert(x); // 1
+
+//                             result = a && b;      
 
