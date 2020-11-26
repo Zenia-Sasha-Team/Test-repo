c866dc7d5985821cc3626c127faa9f5e64d7844b
YevheniiVdovin <32207802+YevheniiVdovin@users.noreply.github.com>
2020-11-26 23:34:44 +0100

    completed 2.14

--- script.js
+++ script.js
@@ -781,3 +781,131 @@
 //     }
 //     alert(i); // простое число
 // }
+
+//                      Конструкция "switch"
+
+//                       Синтаксис
+
+//     switch(x) {
+//     case 'value1':     // if (x === 'value1')
+//       ...
+//       [break]
+//     case 'value2':     // if (x === 'value2')
+//       ...
+//       [break]
+//     default:
+//       ...
+//       [break]
+//   }
+
+//                           Пример
+
+// let a = 2 + 2;
+
+// switch (a) {
+//   case 3:
+//     alert( 'Маловато' );
+//     break;
+//   case 4:
+//     alert( 'В точку!' );
+//     break;
+//   case 5:
+//     alert( 'Перебор' );
+//     break;
+//   default:
+//     alert( "Нет таких значений" );
+// }
+
+// let a = 2 + 2;
+// switch (a) {
+//   case 3:
+//     alert( 'Маловато' );
+//   case 4:
+//     alert( 'В точку!' );
+//   case 5:
+//     alert( 'Перебор' );
+//   default:
+//     alert( "Нет таких значений" );
+// }
+
+
+// let a = "1";
+// let b = 0;
+// switch (+a) {
+//   case b + 1:
+//     alert("Выполнится, т.к. значением +a будет 1, что в точности равно b+1");
+//     break;
+//   default:
+//     alert("Это не выполнится");
+// }
+
+
+// let a = 1;
+// switch (a) {
+//   case 4:
+//     alert('Правильно!');
+//     break;
+//   case 3: // (*) группируем оба case
+//   case 5:
+//          alert('Неправильно!');
+//           alert("Может вам посетить урок математики?");
+//          break;
+//   default:
+//     alert('Результат выглядит странновато. Честно.');
+// }
+
+
+// let arg = prompt("Введите число?");
+// switch (arg) {
+//   case '0':
+//   case '1':
+//          alert( 'Один или ноль' );
+//          break;
+//   case '2':
+//          alert( 'Два' );
+//          break;
+//   case 3:
+//          alert( 'Никогда не выполнится!' );
+//          break;
+//   default:
+//          alert( 'Неизвестное значение' );
+// }
+
+
+//                          Task
+
+// 1.
+// if (browser === 'Edge') {
+//         alert("You've got the Edge");
+// } else if (browser == 'Chrome'
+//     || browser == 'Firefox' 
+//     || browser == 'Safari' 
+//     || browser == 'Opera') {
+//     alert('Okay we support these browsers too');
+// } else {
+//     alert ('We hope this page looks ok!');
+// }
+
+
+// 2.
+// let num = +prompt('Введите число между 0 и 3','');
+// switch (num) {
+//     case 0:
+//         alert('Вы ввели число 0');
+//         break;
+//     case '1':
+//         alert('Вы ввели число 1');
+//         break
+//     case '2':
+//     case '3':
+//         alert('Вы ввели число 2, а может и 3');
+//         break 
+//     default:
+//         alert('неизвестное значение')
+// }
+
+// + перед чем-либо, например prompt:
+//То что данные введённые в prompt преобразуются в тип Number.
+// !!выражение - преобразовать к Boolean типу.
+// аналог с одним ! - преобразовать данные к Boolean и инвертировать (например: !0 = true).
+// +'10' + +'10' - сложить строки как числа (в этом примере ответ будет 20).
